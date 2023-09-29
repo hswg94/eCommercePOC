e784a33d68deb751d82bd37e116482e7f8a1b166
William Gan <hswg94@gmail.com>
2023-09-29 20:20:24 +0800

    added react-redux and reduxjs for state management

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -12,18 +12,22 @@ import "./assets/styles/index.css";
 import App from "./App.jsx";
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
+import { Provider } from "react-redux"
+import store from "./store.js";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path="/" element={<App/>}>
-      <Route index={true} path="/" element={<HomeScreen/>}/>
-      <Route path="/product/:id" element={<ProductScreen/>}/>
+    <Route path="/" element={<App />}>
+      <Route index={true} path="/" element={<HomeScreen />} />
+      <Route path="/product/:id" element={<ProductScreen />} />
     </Route>
   )
-)
+);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>
 );
--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,31 +1,28 @@
 import { Row, Col } from "react-bootstrap";
 import Product from "../components/Product.jsx";
-import { useState, useEffect } from 'react';
-import axios from 'axios';
+import { useGetProductsQuery } from "../slices/productsApiSlice.js";
 
 const HomeScreen = () => {
-  const [productsData, setProductsData] = useState([]);
-
-  useEffect(()=> {
-    const fetchProducts = async () => {
-      const { data } = await axios.get('/api/products');
-      setProductsData(data);
-    };
-
-    fetchProducts();
-  }, [])
-
+  const { data: productsData, isLoading, isError } = useGetProductsQuery();
 
   return (
     <>
-      <h1>Latest Products</h1>
-      <Row>
-        {productsData.map((product) => (
-          <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-            <Product product={product} />
-          </Col>
-        ))}
-      </Row>
+      {isLoading ? (
+        <h2>Loading...</h2>
+      ) : isError ? (
+        <div>{isError?.data.message || isError.error}</div>
+      ) : (
+        <>
+          <h1>Latest Products</h1>
+          <Row>
+            {productsData.map((product) => (
+              <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                <Product product={product} />
+              </Col>
+            ))}
+          </Row>
+        </>
+      )}
     </>
   );
 };
