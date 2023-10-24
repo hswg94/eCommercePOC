3e9882e8c4abe36cef292f56541898cd77bbc8cb
William Gan <hswg94@gmail.com>
2023-10-24 13:35:22 +0800

    Implemented helmet to improve robustness in security

--- backend/server.js
+++ backend/server.js
@@ -9,14 +9,14 @@ import userRoutes from './routes/userRoutes.js';
 import orderRoutes from './routes/orderRoutes.js';
 import uploadRoutes from './routes/uploadRoutes.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
-
+import helmet from 'helmet';
 
 const port = process.env.PORT || 5000;
 
 connectDB();
 
 const app = express();
-
+app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
