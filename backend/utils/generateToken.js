58a99cb78d2df283792bd0b4dcbe36562fbcc80f
William Gan <hswg94@gmail.com>
2023-11-22 15:08:24 +0800

    Reduced Auth Token Duration

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -2,14 +2,14 @@ import jwt from "jsonwebtoken";
 
 const generateToken = (userId, res) => {
   const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
-    expiresIn: "3d",
+    expiresIn: "0.5d",
   });
 
   res.cookie("jwt", token, {
     httpOnly: true,
     secure: process.env.NODE_ENV !== "development",
     sameSite: "strict",
-    maxAge: 3 * 24 * 60 * 60 * 1000, // 3 days
+    maxAge: 0.5 * 24 * 60 * 60 * 1000, // 0.5 days
   });
 };
 
