c92691686a62a6166f3f362544900d75a699e375
William Gan <hswg94@gmail.com>
2024-01-15 16:59:14 +0800

    reverted api target host

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,8 +8,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        // target: 'http://localhost:5000',
-        target: 'http://hswg94-ecommerce-api.vercel.app:5000',
+        target: 'http://localhost:5000',
         changeOrigin: true
       }
     }
